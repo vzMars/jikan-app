3be133c0be5dfe24f4d16bc4b487309bb31b0bc1
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2022-07-25 16:25:29 -0400

    now able to get top 25 anime and refactored to use async/await and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# simple-jikan-app
+# Jikan App
+
+This app uses the Jikan API to get the current top 25 manga and anime.
+
+**Link to project:** https://vzmars.github.io/jikan-app/
+
+![alt text](https://i.imgur.com/CEcRplf.png)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript
+
+The user has the two button choices that use the Jikan API to fetch an array of objects containing either the top 25 anime or top 25 manga. That array of objects is then rendered onto the page for the user to see.
+
+## Optimizations:
+
+While this app is mobile responsive I would like to go back and improve the look of the app on mobile screens. I would also like to fetch more data using different URLs.
+
+## Lessons Learned:
+
+This was another great opportunity to work with APIs and use async/await. I was able to fetch an array of objects and design an app around the data I got back.
+
+## More Projects:
+
+Take a look at these other projects that I have in my portfolio:
+
+**PokeAPI App:** https://github.com/vzMars/pokeapi-app
+
+**Weather App:** https://github.com/vzMars/weather-app
+
+**Breaking Bad API App:** https://github.com/vzMars/breaking-bad-app
--- js/main.js
+++ js/main.js
@@ -1,51 +1,96 @@
-//getTopAnime - Query Parameters example
-// this example gets the top 5 favorited tv anime starting from page
-// https://api.jikan.moe/v4/top/anime?type=tv&filter=favorite&page=1&limit=5
-const mangaList = document.querySelector('.container');
-const topManga = [];
+const scrollBtn = document.querySelector('#scrollToTop');
+const top25 = document.querySelector('.top-25');
+const mangaBtn = document.querySelector('#manga-btn');
+const animeBtn = document.querySelector('#anime-btn');
+const h1 = document.querySelector('h1');
 
-const getTopManga = () => {
-  fetch('https://api.jikan.moe/v4/top/manga?type=manga')
-    .then((res) => res.json())
-    .then((data) => {
-      topManga.splice(0, data.data.length, ...data.data);
-      createMangaList();
-    })
-    .catch((err) => {
-      console.log(`error: ${err}`);
-    });
-};
+window.addEventListener('scroll', () => {
+  if (window.scrollY > window.outerHeight + window.outerHeight / 2) {
+    scrollBtn.classList.add('active');
+  } else {
+    scrollBtn.classList.remove('active');
+  }
+});
 
-const createMangaList = () => {
-  for (let i = 0; i < topManga.length; i++) {
-    mangaList.append(createManga(topManga[i]));
+scrollBtn.addEventListener('click', () => {
+  // const htmlBody = document.querySelector('html, body');
+  // htmlBody.animate({ sc: 0 }, 500);
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+mangaBtn.addEventListener('click', async () => {
+  top25.replaceChildren();
+  h1.textContent = 'Top 25 Manga';
+  const series = await getTopManga();
+  createSeriesList(series);
+});
+
+animeBtn.addEventListener('click', async () => {
+  top25.replaceChildren();
+  h1.textContent = 'Top 25 Anime';
+  const series = await getTopAnime();
+  createSeriesList(series);
+});
+
+const getTopManga = async () => {
+  try {
+    const response = await fetch(
+      'https://api.jikan.moe/v4/top/manga?type=manga'
+    );
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.log(error);
   }
 };
 
-const createManga = (manga) => {
-  const mangaContainer = document.createElement('div');
-  const mangaInfo = document.createElement('div');
+const getTopAnime = async () => {
+  try {
+    const reponse = await fetch('https://api.jikan.moe/v4/top/anime?type=tv');
+    const data = await reponse.json();
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createSeriesList = (series) => {
+  for (let i = 0; i < series.length; i++) {
+    top25.append(createSeries(series[i]));
+  }
+};
+
+const createSeries = (series) => {
+  const seriesContainer = document.createElement('div');
+  const seriesInfo = document.createElement('div');
   const img = document.createElement('img');
   const title = document.createElement('h2');
-  const author = document.createElement('span');
-  const published = document.createElement('span');
+  const authorStudio = document.createElement('span');
+  const airedPublished = document.createElement('span');
   const score = document.createElement('span');
   const synopsis = document.createElement('p');
 
-  mangaContainer.className = 'manga';
-  mangaContainer.id = manga.rank;
-  mangaInfo.className = 'info';
+  seriesContainer.className = 'series';
+  seriesInfo.className = 'info';
 
-  img.src = manga.images.webp.image_url;
-  title.textContent = manga.title_english;
-  author.textContent = manga.authors[0].name.split(',').reverse().join(' ');
-  published.textContent = manga.published.string.replaceAll('to', '-');
-  score.textContent = manga.score;
-  synopsis.textContent = manga.synopsis;
+  seriesContainer.id = series.rank;
+  img.src = series.images.webp.image_url;
+  title.textContent = series.title;
+  score.textContent = series.score;
+  synopsis.textContent = series.synopsis;
 
-  mangaInfo.append(title, author, published, score, synopsis);
-  mangaContainer.append(img, mangaInfo);
-  return mangaContainer;
+  if (series.url.includes('manga')) {
+    authorStudio.textContent = series.authors[0].name
+      .split(',')
+      .reverse()
+      .join(' ');
+    airedPublished.textContent = series.published.string.replaceAll('to', '-');
+  } else {
+    authorStudio.textContent = series.studios[0].name;
+    airedPublished.textContent = series.aired.string;
+  }
+
+  seriesInfo.append(title, authorStudio, airedPublished, score, synopsis);
+  seriesContainer.append(img, seriesInfo);
+  return seriesContainer;
 };
-
-getTopManga();
